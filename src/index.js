5f1cb33519311638c1f9cfeed5c6d51ab55d69eb
Margaret <ivonne.mitchellf@gmail.com>
2026-09-16 15:16:00 -0300

    feat: add src/index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+export { Lexer, tokenize } from "./lexer.js";
+export { Parser, parse } from "./parser.js";
+export { TokenType, MargaCodeError } from "./token.js";
+export {
+  MargaTypes,
+  EMOTIONAL_STATES,
+  PREFERRED_FREQUENCIES,
+  PROTECTION_LEVELS,
+  validateTypeValue
+} from "./types.js";
+export { validate } from "./validate.js";
